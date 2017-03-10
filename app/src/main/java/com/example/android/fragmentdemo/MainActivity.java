d5c271f574f0f9e91578d24244f581de49d31e24
Your Name <giyoon21c@gmail.com>
2017-03-09 16:39:12 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.fragmentdemo;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyFragment frag = new MyFragment();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.activity_main, frag, "XXXFragment");
+        transaction.commit();
+    }
+}
